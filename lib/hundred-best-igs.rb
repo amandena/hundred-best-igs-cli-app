83383c401665bb50db73c64f5f0d4728d5311dbe
amanda johnson <manda.panda91@yahoo.com>
2018-01-04 19:44:03 +0000

    all files require each other correctly

---
+++ lib/hundred-best-igs.rb
@@ -0,0 +1,4 @@
+require_relative './hundred-best-igs/version'
+require_relative './hundred-best-igs/cli.rb'
+require_relative './hundred-best-igs/best-igs.rb'
+require_relative './hundred-best-igs/scraper.rb'
---
+++ lib/hundred-best-igs/cli.rb
@@ -0,0 +1,6 @@
+class HundredBestIgs::CLI
+
+  def call
+    puts "100 Best Instagram Accounts:"
+  end
+end
