4c6a3e24ae377ef1bb586f95c1a6529370d5d5c7
Amanda Johnson <manda.panda91@yahoo.com>
2018-02-16 17:35:49 -0500

    Update hundred-best-igs.rb

--- lib/hundred-best-igs.rb
+++ lib/hundred-best-igs.rb
@@ -5,4 +5,4 @@ require 'pry'
 require_relative './hundred-best-igs/version'
 require_relative './hundred-best-igs/cli.rb'
 require_relative './hundred-best-igs/best-igs.rb'
-require_relative './hundred-best-igs/scraper.rb'
+
